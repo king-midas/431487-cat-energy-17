7e0ee8f26c007e63a0fb491597a48e834d0b6300
king-midas <1nformer294@gmail.com>
2019-08-05 14:06:34 +0300

    меняет link на showButton

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,7 @@ var showButton = document.querySelector(".js-main-nav__toggle");
 
 var navMenu = document.querySelector(".main-nav__wrapper");
 
-link.addEventListener("click", function(evt){
+showButton.addEventListener("click", function(evt){
   evt.preventDefault();
   navMenu.classList.add("js-main-nav--open ");
 });
